22ac064ddcfcf1b449507fe0f36836c174842b55
Arnabh <sinhaarnabh888@gmail.com>
2023-11-16 21:55:44 +0530

    new commit

--- upi_bus/src/pages/Home.js
+++ upi_bus/src/pages/Home.js
@@ -1,11 +1,14 @@
-import './Home.css'
+import './Home.css';
+import { useNavigate } from 'react-router-dom';
+
 export default function Home() {
+  const navigate= useNavigate();
   return (
     <div className='main'>
       <div >
       <img className='img' src="https://assets.telegraphindia.com/telegraph/2022/Feb/1644781493_bus.jpg" alt="" />
       </div>
-    <button className="btn">
+    <button onClick={()=>navigate("/BookTickets")} className="btn">
       BOOK YOUR TICKETS HERE
     </button>
 
